b2f97e86251890d4023659c191a668f763cabd4a
Masahide Matsumoto <matsumoto.masahide@gmail.com>
2020-06-15 21:53:20 +0900

    attribute that have omitempty tag have to be pointer.

--- iotagent/service_group.go
+++ iotagent/service_group.go
@@ -22,19 +22,19 @@ type (
 	}
 
 	ServiceGroup struct {
-		Service            string            `json:"service,omitempty" bson:"service"`                         // Service of the devices of this type
-		SubService         string            `json:"subservice,omitempty" bson:"subservice"`                   // Subservice of the devices of this type.
-		Resource           string            `json:"resource" bson:"resource"`                                 // string representing the Southbound resource that will be used to assign a type to a device (e.g.: pathname in the southbound port).
-		APIKey             string            `json:"apikey" bson:"apikey"`                                     // API Key string.
-		TimeStamp          bool              `json:"timestamp,omitempty" bson:"timestamp"`                     // Optional flagw whether to include the TimeInstantwithin each entity created, as well as a TimeInstant metadata to each attribute, with the current timestamp
-		EntityType         string            `json:"entity_type" bson:"entity_type"`                           // name of the Entity type to assign to the group.
-		Trust              string            `json:"trust,omitempty" bson:"trust"`                             // trust token to use for secured access to the Context Broker for this type of devices (optional; only needed for secured scenarios).
-		CbHost             string            `json:"cbHost" bson:"cbHost"`                                     // Context Broker connection information. This options can be used to override the global ones for specific types of devices.
-		Lazy               []DeviceAttribute `json:"lazy,omitempty" bson:"lazy"`                               // list of common lazy attributes of the device. For each attribute, its name and type must be provided.
-		Commands           []DeviceAttribute `json:"commands,omitempty" bson:"commands"`                       // list of common commands attributes of the device. For each attribute, its name and type must be provided, additional metadata is optional.
-		Attributes         []DeviceAttribute `json:"attributes,omitempty" bson:"attributes"`                   // list of common active attributes of the device. For each attribute, its name and type must be provided, additional metadata is optional.
-		StaticAttributes   []DeviceAttribute `json:"static_attributes,omitempty" bson:"static_attributes"`     // this attributes will be added to all the entities of this group 'as is', additional metadata is optional.
-		InternalAttributes []interface{}     `json:"internal_attributes,omitempty" bson:"internal_attributes"` // optional section with free format, to allow specific IoT Agents to store information along with the devices in the Device Registry.
+		Service            *string            `json:"service,omitempty" bson:"service"`                         // Service of the devices of this type
+		SubService         *string            `json:"subservice,omitempty" bson:"subservice"`                   // Subservice of the devices of this type.
+		Resource           string             `json:"resource" bson:"resource"`                                 // string representing the Southbound resource that will be used to assign a type to a device (e.g.: pathname in the southbound port).
+		APIKey             string             `json:"apikey" bson:"apikey"`                                     // API Key string.
+		TimeStamp          *bool              `json:"timestamp,omitempty" bson:"timestamp"`                     // Optional flagw whether to include the TimeInstantwithin each entity created, as well as a TimeInstant metadata to each attribute, with the current timestamp
+		EntityType         string             `json:"entity_type" bson:"entity_type"`                           // name of the Entity type to assign to the group.
+		Trust              *string            `json:"trust,omitempty" bson:"trust"`                             // trust token to use for secured access to the Context Broker for this type of devices (optional; only needed for secured scenarios).
+		CbHost             string             `json:"cbHost" bson:"cbHost"`                                     // Context Broker connection information. This options can be used to override the global ones for specific types of devices.
+		Lazy               *[]DeviceAttribute `json:"lazy,omitempty" bson:"lazy"`                               // list of common lazy attributes of the device. For each attribute, its name and type must be provided.
+		Commands           *[]DeviceAttribute `json:"commands,omitempty" bson:"commands"`                       // list of common commands attributes of the device. For each attribute, its name and type must be provided, additional metadata is optional.
+		Attributes         *[]DeviceAttribute `json:"attributes,omitempty" bson:"attributes"`                   // list of common active attributes of the device. For each attribute, its name and type must be provided, additional metadata is optional.
+		StaticAttributes   *[]DeviceAttribute `json:"static_attributes,omitempty" bson:"static_attributes"`     // this attributes will be added to all the entities of this group 'as is', additional metadata is optional.
+		InternalAttributes *[]interface{}     `json:"internal_attributes,omitempty" bson:"internal_attributes"` // optional section with free format, to allow specific IoT Agents to store information along with the devices in the Device Registry.
 	}
 )
 
@@ -42,8 +42,8 @@ type (
 func (a *Accessor) CreateServiceGroup(service, path string, body *APIServiceGroup) error {
 	// set service and service path into request.
 	for i := range body.Services {
-		body.Services[i].Service = service
-		body.Services[i].SubService = path
+		body.Services[i].Service = &service
+		body.Services[i].SubService = &path
 	}
 	req, err := gohttp.GenRequest(gohttp.HttpMethods.POST, a.genConfigUrl(ServiceGroupUrl), body)
 	if err != nil {
