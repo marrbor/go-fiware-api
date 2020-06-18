25c366c96479f8d11c2f804bb3f54d525e3860d3
Masahide Matsumoto <matsumoto.masahide@gmail.com>
2020-06-18 20:58:10 +0900

    Add NGSI type definition.

---
+++ ngsi/types.go
@@ -0,0 +1,16 @@
+// Type definition
+package ngsi
+
+// Data Type
+// https://quantumleap.readthedocs.io/en/latest/user/
+const (
+	NgsiArray       = "Array"
+	Boolean         = "Boolean"
+	DateTime        = "DateTime"
+	Integer         = "Integer"
+	GeoPoint        = "geo:point"
+	GeoJson         = "geo:json"
+	Number          = "Number"
+	Text            = "Text"
+	StructuredValue = "StructuredValue"
+)
